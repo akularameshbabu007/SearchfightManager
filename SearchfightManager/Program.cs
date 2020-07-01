da6de370bd73f68b55b05c058c13b7d84a97bc3d
ramesh <rameshsoftcsc@gmail.com>
2020-07-02 03:51:44 +0530

    Add project files.

---
+++ SearchfightManager/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SearchfightManager
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var queryParams = "?args=";
+
+            foreach (var argument in args)
+            {
+                queryParams = queryParams + argument;
+            }
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i == 0)
+                {
+                    queryParams = "?args=" + args[i];
+                }
+                else
+                {
+                    queryParams = queryParams + "&args=" + args[i];
+                }
+            }
+            HttpResponseMessage response = await client.GetAsync(@"https://localhost:44331/search" + queryParams);
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
